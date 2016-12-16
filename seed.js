b1e81f3916daa1292740d14e6061ce2d4fd593a5
vedelopment <vedelopment@gmail.com>
2016-12-16 05:54:04 -0800

    updates to debug further, artists seed still not showing up

--- seed.js
+++ seed.js
@@ -3,16 +3,23 @@
 
 var db = require('./models');
 
-var artist_list = [
+var artists_list = [
   {
   name: "Scott Walker",
-  image: "http://ksassets.timeincuk.net/wp/uploads/sites/55/2016/07/2014ScottWalker_Press_020714-1.jpg"
+  image: "http://ksassets.timeincuk.net/wp/uploads/sites/55/2016/07/2014ScottWalker_Press_020714-1.jpg",
   genre: "Experimental",
   soloArtist: true,
   favoriteAlbum: "The Drift",
-  alive: true,
+  alive: true
   },
-]
+  name: "The Pharcyde",
+  image: "https://ichef.bbci.co.uk/images/ic/960x540/p01bqp2z.jpg",
+  genre: "Alternative Hip-Hop",
+  soloArtist: false,
+  favoriteAlbum: "Bizarre Ride II",
+  alive: true
+  },
+];
 
 db.Artist.remove({}, function(err, artists){
   if(err) {
@@ -21,7 +28,7 @@ db.Artist.remove({}, function(err, artists){
     console.log('removed all artists');
 
     // create new records based on the array books_list
-    db.Artist.create(artist_list, function(err, artists){
+    db.Artist.create(artists_list, function(err, artists){
       if (err) { return console.log('err', err); }
       console.log("created", artists.length, "artists");
       process.exit();

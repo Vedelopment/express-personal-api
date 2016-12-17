d34274c190fabb345df021d4db6276eefa6c5dbe
vedelopment <vedelopment@gmail.com>
2016-12-17 15:48:51 -0800

    add styling and animations to contain databse entries

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,12 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
-
-// your code
-
+  $('.list-anim').click(function showProjectsList() {
+    var thing = this;
+    $(thing).find('ul').toggleClass('list-show');
+    $(thing).find('li').toggleClass('typewriter');
+    // setTimeout(function() {
+    //     $(thing).find('li').stop(true, true).removeClass('typewriter');
+    //   }, 4000);
+  });
 });

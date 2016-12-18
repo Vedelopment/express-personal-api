74401e8c08e43cbb58287277e2111d82c8208ebc
vedelopment <vedelopment@gmail.com>
2016-12-17 16:58:28 -0800

    updates style animations more because I'm still avoiding back end

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,9 +4,6 @@ $(document).ready(function(){
   $('.list-anim').click(function showProjectsList() {
     var thing = this;
     $(thing).find('ul').toggleClass('list-show');
-    $(thing).find('li').toggleClass('typewriter');
-    // setTimeout(function() {
-    //     $(thing).find('li').stop(true, true).removeClass('typewriter');
-    //   }, 4000);
+    $(thing).find('span').toggleClass('typewriter');
   });
 });
